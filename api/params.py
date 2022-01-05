c31277f765024628f36734ec66f2e3ca3d195964
tinou_99 <tinou99.f@gmail.com>
2022-01-05 20:46:53 +0100

    new: params.py created

---
+++ api/params.py
@@ -0,0 +1,14 @@
+desc_seoul_bike = 'Currently Rental bikes are introduced in many urban cities for the enhancement of mobility ' \
+                  'comfort. It is important to make the rental bike available and accessible to the public at the ' \
+                  'right time as it lessens the waiting time. Eventually, providing the city with a stable supply of ' \
+                  'rental bikes becomes a major concern. The crucial part is the prediction of bike count required ' \
+                  'at each hour for the stable supply of rental bikes. The dataset contains weather information ' \
+                  '(Temperature, Humidity, Windspeed, Visibility, Dewpoint, Solar radiation, Snowfall, Rainfall), ' \
+                  'the number of bikes rented per hour and date information. '
+
+desc_blocks_classification = 'There are 5473 examples coming from 54 distinct documents and each observation ' \
+                             'concerns one block. The problem consists in classifying all the blocks of the page ' \
+                             'layout of a document that has been detected by a segmentation process. This is an ' \
+                             'essential step in document analysis in order to separate text from graphic areas. ' \
+                             'There are five classes: Text (1), Horizontal line (2), Picture (3), Vertical line (4) ' \
+                             'Graphic (5).'
